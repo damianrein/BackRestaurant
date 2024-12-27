25d4080a2679bd4e5a58f6f98b537ec5a823b6b8
damianrein <damianrein464@gmail.com>
2024-12-26 22:42:11 -0300

    -

---
+++ src/main/java/com/BackRestaurant/BackRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.BackRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BackRestaurant/BackRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
