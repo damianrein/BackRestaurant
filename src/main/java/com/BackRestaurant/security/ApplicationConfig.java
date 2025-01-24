857906d10bfb94b41a3bfd219dd29feca42ee562
damianrein <damianrein464@gmail.com>
2025-01-23 21:31:16 -0300

    -

---
+++ src/main/java/com/BackRestaurant/security/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.BackRestaurant.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+}
--- src/main/java/com/BackRestaurant/security/SecurityConfig.java
+++ src/main/java/com/BackRestaurant/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.BackRestaurant.security;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfig {
 
 }
